5f2d243153e08331c8470484428d77dcf0068e62
phuong <ntkphuong020392@gmail.com>
2024-05-01 17:50:03 +0700

    Index page

---
+++ CozaStore/src/main/java/com/coza/controller/user/HomeController.java
@@ -0,0 +1,14 @@
+package com.coza.controller.user;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/index")
+    public String showIndex(){
+        return "user/index";
+    }
+}
